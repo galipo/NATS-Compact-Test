3cdede90646addd1ea5b65f0380988bba52246e1
Cagkansari <cagkansari@hotmail.com>
2023-09-05 09:27:58 +0300

    Compact with publish and functions, exit option  included.

--- TestCompact.c
+++ TestCompact.c
@@ -50,10 +50,52 @@ int main(int argc, char **argv)
     char subject[255];
     char serverAdress[255];
     char message[255];
+    enum Operation op;
+    int exit = 1;
 
     s = natsOptions_Create(&opts);
     s = natsConnection_ConnectTo(&conn, changeServer(serverAdress));
-    s = natsConnection_PublishString(conn, changeSubject(subject), publishMsg(message));
+
+    
+    while (exit)    
+    {
+        printf("Select operation number: \n 0.Publish \n 1.Subscribe \n 4.Exit \n");
+        scanf("%u", &op);
+
+         switch (op)
+        {
+        case Publish:   s = natsConnection_PublishString(conn, changeSubject(subject), publishMsg(message)); break;
+        case Subscribe: s = natsConnection_SubscribeSync(&sub, conn, changeSubject(subject));
+                        s = natsSubscription_NextMsg(&msg, sub, 10000);
+                         if (s == NATS_OK)   
+                            {   
+                                printf("Received msg: %s - %.*s\n",
+                                    natsMsg_GetSubject(msg),
+                                    natsMsg_GetDataLength(msg),
+                                    natsMsg_GetData(msg));
+
+                            //Created things must be destroyed
+                                natsMsg_Destroy(msg);
+                            }
+                            //Message receiving time interval
+                            else if (s == NATS_TIMEOUT)
+                            {
+                                printf("No message received within the timeout.\n");
+                            }
+                        natsSubscription_Destroy(sub); 
+        break;
+        case ExitProgram: exit = 0; break;
+    
+        default:
+         break;
+        }
+    }
+    
+   
+
+    
+    
+   
     natsConnection_Destroy(conn);
     natsOptions_Destroy(opts);
 
